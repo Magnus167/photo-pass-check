fdcb94cd71f2e63bab7c60c3db5af72a98e9e1ac
Spalash <palashtyagi167@gmail.com>
2022-05-29 23:14:41 +0100

    .

--- README.md
+++ README.md
@@ -7,5 +7,11 @@ Setup the script to run on startup with cron/Task Scheduler.
 Please [buy me a coffee](https://www.buymeacoffee.com/pt420) lol. 
 
 
+## Future Plans:
+
+Add a way to upload the photo to a Google Drive/cloud folder immediately after taking it.
+A dummy method already added, but it doesn't work so is enclosed in a try/except block.
+
+
 
 
--- login_face_capture.py
+++ login_face_capture.py
@@ -1,18 +1,47 @@
 import cv2, os
 from datetime import datetime
-picturePath = "./login_pictures/" 
-if os.path.exists(picturePath) == False:
-    os.mkdir(picturePath)
 
-cam_port = 0
-cam = cv2.VideoCapture(cam_port)
+def uploadFile(fName, driveFolderLink):
+    folderID = driveFolderLink.split('/')[-1]
+    from pydrive.auth import GoogleAuth
+    from pydrive.drive import GoogleDrive
 
-result, image = cam.read()
+    gauth = GoogleAuth()           
+    drive = GoogleDrive(gauth)  
 
-if result:
-    ''' imgName is hh-mm-ss--DD-MMM-YYYY '''
-    cv2.imwrite(picturePath + datetime.now().strftime("%H-%M-%S--%d-%b-%Y") + ".jpg", image)
-else:
-    print("No camera detected")
-    with open(picturePath + datetime.now().strftime("%H-%M-%S--%d-%b-%Y") + ".txt", "w") as f:
-        f.write("No camera detected")
+    gfile = drive.CreateFile({'parents': [{'id': folderID}]})
+    
+    gfile.SetContentFile(fName)
+    gfile.Upload() 
+    return gfile.GetLink()
+
+
+
+def main():
+    picturePath = "./login_pictures/" 
+    if os.path.exists(picturePath) == False:
+        os.mkdir(picturePath)
+
+    cam_port = 0
+    cam = cv2.VideoCapture(cam_port)
+
+    result, image = cam.read()
+
+    if result:
+        ''' imgName is hh-mm-ss--DD-MMM-YYYY '''
+        fName = picturePath + datetime.now().strftime("%H-%M-%S--%d-%b-%Y") + ".jpg"
+        cv2.imwrite(fName, image)
+        # link = uploadFile(fName, "https://drive.google.com/drive/u/1/folders/1TD-mqni916vEPCVGCRmvs3gwq9ycmjz0")
+        try:
+            link = uploadFile(fName, "https://drive.google.com/drive/u/1/folders/1TD-mqni916vEPCVGCRmvs3gwq9ycmjz0")
+            print("Link: " + link)
+        except:
+            print("Error uploading file")
+        
+    else:
+        print("No camera detected")
+        with open(picturePath + datetime.now().strftime("%H-%M-%S--%d-%b-%Y") + ".txt", "w") as f:
+            f.write("No camera detected")
+
+if __name__ == "__main__":
+    main()
